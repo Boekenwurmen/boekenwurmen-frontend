21575db804295d8ce4734890be9d9c6260ebed9c
Boaz Crezee <bjcrezee@gmail.com>
2025-11-28 14:36:45 +0100

    added test cases to check if option links are positive integer and option text is not an empty string, refactored "indexes", and renamed "element" to "page"

--- src/lib/components/Stories.svelte.test.js
+++ src/lib/components/Stories.svelte.test.js
@@ -1,23 +1,31 @@
 import { expect, test } from 'vitest';
 import Stories from './Stories.svelte.js';
 
-test('Stories', () => {
-    const indexes = [-10000, -100, -10, -3, -2, -1, 0, 0.5, 1, 2, 3, 10, 100, 1000, 10000, Infinity, undefined, null];
-    indexes.forEach(element => {
-        const story = Stories.getPageStory(element);
+const indexes = [-10000, -100, -10, -3, -2, -1, 0, 0.5, 1, 2, 3, 10, 100, 1000, 10000, Infinity, undefined, null];
+
+test("All pages give valid stories", () => {
+    indexes.forEach(page => {
+        const story = Stories.getPageStory(page);
         expect(story).toBeTypeOf('string');
         expect(story).not.toBe('');
         // expect(story).toMatch('');
     });
 });
 
-test('Options', () => {
-    const indexes = [-10000, -100, -10, -3, -2, -1, 0, 0.5, 1, 2, 3, 10, 100, 1000, 10000, Infinity, undefined, null];
-    indexes.forEach(element => {
-        const options = Stories.getPageOptions(element);
+test('All pages give valid options', () => {
+    indexes.forEach(page => {
+        const options = Stories.getPageOptions(page);
         expect(options).toBeInstanceOf(Array);
         options.forEach(option => {
-            expect(option).toEqual(expect.objectContaining({ toPage: expect.any(Number), name: expect.any(String) }));
+            expect(option).toEqual(
+                expect.objectContaining({
+                    toPage: expect.any(Number),
+                    name: expect.any(String)
+                })
+            );
+            expect(option.toPage).toBeGreaterThan(0);
+            expect(Number.isInteger(option.toPage)).toBe(true);
+            expect(option.name).not.toBe('');
         });
     });
 });
