c573bda375f0ba43213ac8bb9c4fd37abf974141
Boaz Crezee <bjcrezee@gmail.com>
2025-12-05 13:04:57 +0100

    toevoegen van beschrijving-comments en jsdocs en verduidelijken van namen in readingSettings

--- src/lib/components/ReadingSettings.svelte
+++ src/lib/components/ReadingSettings.svelte
@@ -6,14 +6,17 @@
      * @type {{speed:number, myTypeWriter:TypeWriter}}
      */
     const readingSettingsContext = getContext('readingSettings');
-    let readingSpeed = 5;
+    let readingSpeed = readingSettingsContext.speed;
     
     onMount(() => {
-        // when page is loaded
+        // when page is loaded, run set reading speed again to force the range input element to show this value
         readingSpeed = readingSettingsContext.speed;
     });
 
-    function handleInput() {
+    /**
+     * sets the reading speed when it changes
+     */
+    function handleReadingSpeedUpdate() {
         readingSettingsContext?.myTypeWriter?.setSpeed(readingSpeed);
         readingSettingsContext.speed = readingSpeed;
     }
@@ -24,7 +27,7 @@
         <summary>Settings</summary>
         <div>
             <label for="readingSpeed">Reading Speed</label>
-            <input type="range" id="readingSpeed" name="readingSpeed" min="5" max="80" step="5" bind:value={readingSpeed} on:input={handleInput}/>
+            <input type="range" id="readingSpeed" name="readingSpeed" min="5" max="80" step="5" bind:value={readingSpeed} on:input={handleReadingSpeedUpdate}/>
         </div>
     </details>
 </div>
