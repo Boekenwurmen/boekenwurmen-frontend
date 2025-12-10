bf4693d5065504d7c5260c08ab338aed615666ca
Boaz Crezee <bjcrezee@gmail.com>
2025-12-10 21:45:03 +0100

    vervangen lokaal verhaal met een lokaal jsonbestand

--- src/lib/components/Stories.svelte.js
+++ src/lib/components/Stories.svelte.js
@@ -1,4 +1,5 @@
 import { PUBLIC_API_URL } from "$env/static/public";
+import { _getPageOptionsJson, _getPageStoryJson } from "$lib/algorithms/bookJsonAccess.js";
 
 export default class Stories {
     /**
@@ -57,7 +58,6 @@ export default class Stories {
         } else {
             throw new Error('backend did not return the story as a string');
         }
-
     }
 
     /**
@@ -105,49 +105,25 @@ export default class Stories {
 
     /**
      * 
-     * @param page {number | null | undefined}
+     * @param {number | null | undefined} page
      */
     static _getPageStoryLocal(page){
-        switch (page) {
-            case 0: return "You walk through a sandy, windy desert. The wind blows sand in your eyes and you fall to the ground."
-            case 1: return "You start screaming into the vast empty desert. While screaming you feel yourself slowely sinking into the ground when all of a sudden you fall through the ground! You hit the ground hard and hurt yourself."
-            case 2: return "As soon as you hit the ground you feel a wooden hatch beneath you. It feels old and rotten. Yet when you put your ear to the hatch you hear running water. As you hear the water your throat starts to dry up you have not drank anything for about a day..."
-            case 3: return "You enter what seems to be a library with a bit of running water. You take a drink and start looking around but all the books you see you can't read..."
-            case 4: return "As you wander further you become more and more thirsty and start seeing things that aren’t there... After five more hours of wandering in the heat of the desert you fall to the ground. You fall asleep not to wake up again... "
-            case 5: return "As you look around you see the running water and take a sip. behind all the water you see a old library filled with books you can’t read..."
-            case 6: return "You check your leg and see it bleeding. You rip off a piece of your shirt and after you clean the wound with the running water you see, you make a makeshift bandage. Now that you’ve treated your wound you start to look around and see an old library filled with books you can’t read...."
-            default: return `This part of the story ${Math.random() < 0.5 ? 'went missing' : 'got burned up'}.`
+        if (page === null || page === undefined) {
+            return `This part of the story ${Math.random() < 0.5 ? 'went missing' : 'got burned up'}.`;
         }
+        return _getPageStoryJson(0, page);
     }
 
     /**
      * 
-     * @param page {number | null | undefined}
+     * @param {number | null | undefined} page 
      */
     static _getPageOptionsLocal(page){
-        switch (page) {
-
-            case 0: return [
-                {toPage:2, name:"You hit the ground out of frustation"},
-                {toPage:1, name:"You scream in pain from the sand in your eyes"},
-            ]
-            case 1: return [
-                {toPage:2, name:"You hit the ground out of frustration"},
-                {toPage:2, name:"You scream in pain from the sand in your eyes"},
-            ]
-            case 2: return [
-                {toPage:3, name:"Check yourself for wounds"},
-                {toPage:3, name:"Look around to see where you are"},
-            ]
-            case 3: case 6: case 7: return [
-                {toPage:Infinity, name:"Enter library"},
-            ]
-            case 4: return [
-                {toPage:1, name:"Go back"},
-            ]
-            default: return [
-                {toPage:1, name:"Go back"},
-            ]
+        if (page === null || page === undefined) {
+            return [
+                {toPage:0, name:"Go back"},
+            ];
         }
+        return _getPageOptionsJson(0, page);
     }
 }
