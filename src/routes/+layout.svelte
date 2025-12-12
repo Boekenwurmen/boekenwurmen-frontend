95128e4d41055972b9207e8876a1d80ebf8e0644
Boaz Crezee <bjcrezee@gmail.com>
2025-12-12 15:01:16 +0100

    toevoegen van iconen aan de headerbalk en text wanneer je erop hovert met een muis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import leaderboard from '$lib/assets/leaderboard.svg';
+	import library from '$lib/assets/library.svg';
+	import reading from '$lib/assets/reading.svg';
 
 	let { children } = $props();
 </script>
@@ -10,15 +13,26 @@
 </svelte:head>
 
 
-<header class="bg-yellow-700 grid grid-cols-4 gap-4">
+<header class="bg-yellow-700 flex justify-start align-center gap-4">
 	<a href="/">
-		<img src={favicon} alt="Library logo" class="header-icon w-30 h-30" />
+		<abbr title="Thuismenu">
+			<img src={favicon} alt="Library logo" class="header-icon w-30 h-30" />
+		</abbr>
 	</a>
 	<a href="/leaderboard" class="button button--small" aria-label="Toon ranglijst">
-		Leaderboard
+		<abbr title="Ranglijst">
+			<img src={leaderboard} alt="Leaderboard logo" class="header-icon w-20 h-20" />
+		</abbr>
 	</a>
-	<a href="/library" class="button button--small" aria-label="Toon ranglijst">
-		Library
+	<a href="/library" class="button button--small" aria-label="Toon bibliotheek">
+		<abbr title="Bibliotheek">
+			<img src={library} alt="Library logo" class="header-icon w-20 h-20" />
+		</abbr>
+	</a>
+	<a href="/read" class="button button--small" aria-label="Toon leespagina">
+		<abbr title="Lezen">
+			<img src={reading} alt="Reading logo" class="header-icon w-20 h-20" />
+		</abbr>
 	</a>
 </header>
 <div class="book-page-container">
