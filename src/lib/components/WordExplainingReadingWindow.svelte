80e21c85238268bed778bac0230cdc321d5bb151
Boaz Crezee <bjcrezee@gmail.com>
2025-12-17 18:19:58 +0100

    turned WordExplainingReadingWindow.svelte to typescript

--- src/lib/components/WordExplainingReadingWindow.svelte
+++ src/lib/components/WordExplainingReadingWindow.svelte
@@ -1,8 +1,8 @@
-<script>
+<script lang="ts">
 	import WordExplainer from "./WordExplainer.svelte";
     // import Snippet from "./WordExplainer";
 
-    const {/** @type { string }*/ text = ""} = $props();
+    const {text = "" as string} = $props();
 
     // $effect(() => {
     //     // snapshot primitives from proxied $state
@@ -10,26 +10,24 @@
     //     // const bookId = pageContext ? Number(pageContext[1]) : undefined;
     // })
 
-    /**
-     * @typedef {Object} WordDefinition
-     * @property {string} wordType - The grammatical type of the word (e.g., noun, verb, adjective)
-     * @property {string} definition - The definition of the word
-     * @property {string} example - An example sentence using the word
-     *
-     * @typedef {Object} Word
-     * @property {string} word - The word text
-     * @property {WordDefinition} wordDefinition - The definition details for the word
-     *
-     * @typedef {Object} Snippet
-     * @property {string} text - The full text snippet or passage
-     * @property {Word} word - The word object containing the word and its definition
-     */
+    interface WordDefinition {
+        wordType: string;
+        definition: string;
+        example: string;
+    }
+
+    interface Word {
+        word: string;
+        wordDefinition: WordDefinition;
+    }
+
+    interface Snippet {
+        text: string;
+        word: Word;
+    }
 
     const initialText = $derived(text);
-    /**
-     * @type {Snippet[]}
-     */
-    const snippets = [
+    const snippets:Snippet[] = [
         // {
         //     text: 'bbb',
         //     word: {
