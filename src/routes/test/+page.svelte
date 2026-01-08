1ae9adc3f1d26de431fe0654dd419ba63ca03859
Boaz Crezee <bjcrezee@gmail.com>
2026-01-08 18:37:40 +0100

    removed test route and test book cover image

--- src/routes/test/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import Book3d from "../../lib/components/book3d.svelte";
-</script>
-
-<!-- make sure the image resolution is 250 x 375 -->
-<Book3d image={'/static/covers/test.svg'} url={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}/>
