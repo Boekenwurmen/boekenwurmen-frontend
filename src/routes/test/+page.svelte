bf9aafb9f89aabc39574da1f88d1798e4d62f2d1
Boaz Crezee <bjcrezee@gmail.com>
2026-01-08 17:51:37 +0100

    imported a 3d book effect from refactoring.guru

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Book3d from "../../lib/components/book3d.svelte";
+</script>
+
+<!-- <Book3d image={'./assets/web-cover-en.png'}/> -->
+<Book3d image={'/static/covers/kosmisch.svg'}/>
